a39df6d4552826d4017954b325f4b54a336d8bbb
Hoàng Lê Nam <116867818+HLeNam@users.noreply.github.com>
2025-12-24 23:35:01 +0700

    Change cookieParser import to use wildcard syntax

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform/transform.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
+// import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
